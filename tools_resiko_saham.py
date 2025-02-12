9fe723a19d604b145e7d0f8228935bc0a913371c
Dimas Saifullah Ramadhan <37479804+dimasRamadhan2398@users.noreply.github.com>
2025-02-12 16:54:10 +0700

    Update tools_resiko_saham.py

--- tools_resiko_saham.py
+++ tools_resiko_saham.py
@@ -125,7 +125,7 @@ else:
             persen_kerugian = (nilai_kerugian / start_price) * 100
             if ticker1.endswith('.JK'):
                 if harga_kerugian < 0 :
-                    st.subheader(":red[Resiko kerugian] yang bisa Anda alami adalah modal investasi Anda hilang karena harga saham menyentuh :red[dibawah 0]")
+                    st.subheader(":red[Resiko kerugian] yang bisa Anda alami adalah :red[modal investasi Anda hilang / habis] karena harga saham menyentuh :red[dibawah 0]")
                 else:
                     st.write(
                         "Jika Anda membeli saham :blue[%s]" %(ticker1), "di harga sekarang yaitu :blue[Rp%.0f]." %(start_price), "Maka kemungkinan :red[resiko kerugian tertinggi] yang bisa Anda alami dalam",
@@ -137,7 +137,7 @@ else:
                     col2.metric("Predicted Highest Loss Price", "%.0f"%(harga_kerugian), "-%.2f%%"%(persen_kerugian))
             else:
                 if harga_kerugian < 0:
-                    st.subheader(":red[Resiko kerugian] yang bisa Anda alami adalah modal investasi Anda hilang karena harga saham menyentuh :red[dibawah 0]")
+                    st.subheader(":red[Resiko kerugian] yang bisa Anda alami adalah :red[modal investasi Anda hilang / habis] karena harga saham menyentuh :red[dibawah 0]")
                 else:
                     st.write(
                         "Jika Anda membeli saham :blue[%s]" %(ticker1), "di harga sekarang yaitu :blue[Rp%.0f]." %(start_price), "Maka kemungkinan :red[resiko kerugian tertinggi] yang bisa Anda alami dalam",
